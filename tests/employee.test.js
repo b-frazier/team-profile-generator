a76d6fc431dedb8a41012d1761ad218d058ecebb
bfrazier <bthehippie2@gmail.com>
2023-01-10 20:35:29 -0500

    passed getName method test

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -12,7 +12,11 @@ test('returns an employee object', () => {
     expect(empObj.email).toEqual(expect.any(String));
 });
 
+test('getName method returns name', () => {
+    const empObj = new Employee('Bianca', 22, 'bianca.frazier90@gmail.com')
 
+    expect(empObj.getName()).toEqual('Bianca')
+})
 
 
 
