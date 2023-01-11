a34cb84a191c810eea9b82449acb75e1ba2440df
bfrazier <bthehippie2@gmail.com>
2023-01-10 21:26:10 -0500

    write test for github method

---
+++ tests/engineer.test.js
@@ -0,0 +1,6 @@
+const Engineer = require('../lib/engineer');
+const engObj = new Engineer('Phoebe', 8, 'ppnasia99@gmailcom', 'b-frazier');
+
+test('returns github', () => {
+    expect(engObj.getGithub()).toEqual('b-frazier')
+});
