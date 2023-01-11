99238436942bf6e9d5cf30e247c3d4efb0369b5a
bfrazier <bthehippie2@gmail.com>
2023-01-10 20:53:30 -0500

    add getrole test; cleaned code

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,34 +1,31 @@
 const Employee = require('../lib/employee.js');
+const empObj = new Employee('Bianca', 22, 'bianca.frazier90@gmail.com')
 
 test('employee class exists', () => {
     expect(Employee).toBeDefined();
 })
 
 test('returns an employee object', () => {
-    const empObj = new Employee('Bianca', 22, 'bianca.frazier90@gmail.com')
-
     expect(empObj.name).toEqual(expect.any(String));
     expect(empObj.id).toEqual(expect.any(Number));
     expect(empObj.email).toEqual(expect.any(String));
 });
 
 test('getName method returns name', () => {
-    const empObj = new Employee('Bianca', 22, 'bianca.frazier90@gmail.com')
-
     expect(empObj.getName()).toEqual('Bianca')
 });
 
 test('getId method returns Id', () => {
-    const empObj = new Employee('Bianca', 22, 'bianca.frazier90@gmail.com')
-
     expect(empObj.getId()).toEqual(22)
 });
 
 test('getEmail method returns email', () => {
-    const empObj = new Employee('Bianca', 22, 'bianca.frazier90@gmail.com')
-
     expect(empObj.getEmail()).toEqual('bianca.frazier90@gmail.com')
 });
+
+test('getRole returns Employee', () => {
+    expect(empObj.getRole()).toEqual('Employee')
+})
 
 
 
