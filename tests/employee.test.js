963822660834e2b2f3aafc969f1d1ca4be9367a7
bfrazier <bthehippie2@gmail.com>
2023-01-10 19:48:27 -0500

    add id and email to test; passed

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -4,6 +4,8 @@ test('returns an employee object', () => {
     const empObj = new Employee('Bianca', 22, 'bianca.frazier90@gmail.com')
 
     expect(empObj.name).toEqual(expect.any(String));
+    expect(empObj.id).toEqual(expect.any(Number));
+    expect(empObj.email).toEqual(expect.any(String));
 });
 
 
