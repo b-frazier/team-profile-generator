9c74066673e7dea4560b0094a1987b11ae39e8e6
bfrazier <bthehippie2@gmail.com>
2023-01-10 21:27:17 -0500

    add role test

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -4,3 +4,7 @@ const engObj = new Engineer('Phoebe', 8, 'ppnasia99@gmailcom', 'b-frazier');
 test('returns github', () => {
     expect(engObj.getGithub()).toEqual('b-frazier')
 });
+
+test('returns Engineer as role', () => {
+    expect(engObj.getRole()).toEqual('Engineer')
+});
